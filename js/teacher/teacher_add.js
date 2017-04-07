54c2ac4ab120fa63d63fc6b5a456300365a141d5
rain <378152676@qq.com>
2017-04-07 17:28:46 +0800

    自习自己做的功能

--- js/teacher/teacher_add.js
+++ js/teacher/teacher_add.js
@@ -1,3 +1,30 @@
-define(['aside','nprogress'], function(undefined,nprogress) {
-	nprogress.done();
+define(['aside', 'nprogress', 'jquery', 'jqueryCookie'], function (undefined, nprogress, $, undefined) {
+	(function () {
+		console.log($.cookie('teacherInfo').split(','));
+		var teacherInfo=$.cookie('teacherInfo').split(',');
+		console.log(teacherInfo)
+
+		//将cookie里获取的资料渲染到页面上面
+		$('#input-name').val(teacherInfo[1]);
+		$('#date').val(teacherInfo[3]);
+
+		//添加添加时间,点击时发送ajax请求去后台保存资料
+		$('.pull-right').on('click',function(){
+			$.ajax({
+				type:'post',
+				url:'/v6/teacher/add',
+				data:{
+					tc_name:'谭泽栋',
+					tc_pass:'123456',
+					tc_join_date:'2017-01-01',
+					tc_type:1,
+					tc_gender:0
+				},
+				success:function(data){
+					console.log(data)
+				}
+			})
+		})
+			nprogress.done();
+	})()
 });
--- js/teacher/teacher_list.js
+++ js/teacher/teacher_list.js
@@ -1,7 +1,29 @@
-define(['jquery','aside','nprogress','template'], function($,undefined,nprogress,template) {
+define(['jquery','aside','nprogress','template','jqueryCookie'], function($,undefined,nprogress,template,undefined) {
     $.get('/v6/teacher',function(data){
-      console.log(data)
       $('#body').append(template('teacher-list-tpl',data))
+      $('.edit').on('click',function(){
+            console.log($(this).parent().siblings());
+            var trArr = $(this).parent().siblings();
+            var textArr=[];
+          //  var textArr= trArr.map(function(i,v){
+          //     return  v.innerText;
+          //   });
+            // console.log(trArr)
+            for(var i=0;i<trArr.length;i++){
+                   textArr[i]=trArr[i].innerText;
+            }
+            console.log(textArr)
+            // var result =JSON.stringify(textArr)
+            var result =textArr.join();
+            console.log(result)
+            // console.log(JSON.parse(textArr))
+
+            //把结果放进cookie
+            $.cookie('teacherInfo',result,{
+              expires:7,
+              path:'/'              
+            })
+      })
     })
     nprogress.done();
 });
